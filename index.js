d2e2cb4ec919fe262d1a68bf49ad8ec07c0b933e
sirkavics <falf555@gmail.com>
2024-11-14 02:01:01 -0500

    about me and contact modal window and js funcionality

--- index.js
+++ index.js
@@ -26,3 +26,13 @@ function contact(event) {
       );
     });
 }
+
+let isModalOpen = false;
+function toggleModal () {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
